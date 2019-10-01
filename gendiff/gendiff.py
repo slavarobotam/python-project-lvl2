bf621832c540dce586e4f0630c5f59ae165b263b
mari <zefiratakuet@yandex.ru>
2019-10-01 18:11:52 +0300

    Added -f help

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -6,7 +6,7 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
 
 
