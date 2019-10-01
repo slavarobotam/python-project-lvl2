603be73c493fe985c47c480c58ae91bec38f7ba0
mari <zefiratakuet@yandex.ru>
2019-10-01 18:50:20 +0300

    lint fixed 2

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -8,7 +8,7 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
-    args = parser.parse_args()
+    args = parser.parse_args()  # noqa F841
 
 
 if __name__ == '__main__':
