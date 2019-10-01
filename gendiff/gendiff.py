c2d1cc6b0943e8e2eee537907956e39bf03f5e37
mari <zefiratakuet@yandex.ru>
2019-10-01 17:06:28 +0300

    First step. Help displayed

---
+++ gendiff/gendiff.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format')
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
