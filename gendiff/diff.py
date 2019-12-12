74aea0358b86f195ecfaf89e9c0b0a7926032bf5
Mari <zefiratakuet@yandex.ru>
2019-12-12 19:04:07 +0300

    refactoring render

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -6,8 +6,8 @@ from .parsers import get_parsed_data
 
 INDENT = '  '
 NEW, LOST, SAME = '+', '-', ' '
-NESTED = 'N'
-CHANGED = 'CH'
+CHILDREN = 'children'
+CHANGED = 'changed'
 
 
 def main():
@@ -24,65 +24,67 @@ def main():
 
 
 def generate_diff(first, second):  # input two parsed data, output AST
-    same_key_items = {
-        key: {'type': SAME, 'value': first[key]}
-        for key in first
-        if key in second}
-    new_key_items = {
-        key: {'type': NEW, 'value': second[key]}
-        for key in second
-        if key not in first}
-    lost_key_items = {
-        key: {'type': LOST, 'value': first[key]}
-        for key in first
-        if key not in second}
+    same_items = {
+        key: {
+            'type': SAME,
+            'value': first[key]}
+        for key in first if key in second}
+    new_items = {
+        key: {
+            'type': NEW,
+            'value': second[key]}
+        for key in second if key not in first}
+    lost_items = {
+        key: {
+            'type': LOST,
+            'value': first[key]}
+        for key in first if key not in second}
     ast = {}
-    for key in same_key_items:
-        if first[key] == second[key]:
+    for key in same_items:
+        if first[key] == second[key]:  # if item remains the same
             ast[key] = {
                 'type': SAME,
                 'value': first[key]}
         elif isinstance(first[key], dict) and isinstance(second[key], dict):
             ast[key] = {
-                'type': NESTED,
+                'type': CHILDREN,
                 'value': generate_diff(first[key], second[key])}
-        else:
+        else:  # if item was changed
             ast[key] = {
                 'type': CHANGED,
                 'old_value': first[key],
                 'new_value': second[key]}
-    for key in new_key_items:
+    for key in new_items:  # items that were added
         ast[key] = {
             'type': NEW,
             'value': second[key]}
-    for key in lost_key_items:
+    for key in lost_items:  # items that were removed
         ast[key] = {
             'type': LOST,
             'value': first[key]}
     return ast
 
 
-def render(item, level=1):
+def render(data, level=1):
+    if type(data) != dict:
+        return str(data)
     result = []
+    indent = INDENT * level
     if level == 1:
         result.append('{')
-        indent = INDENT
-    else:
-        indent = INDENT * level + INDENT
-    for key, value in item.items():
-        if isinstance(value, dict):
-            if value.get('type') == NESTED:
-                result.append('{}{} {}: {{'.format(indent, SAME, key))
-                result.append(render(value['value'], level + 1))
-                result.append('{}}}'.format(indent + INDENT))
-            elif value.get('type') == CHANGED:
-                result.append('{}{} {}: {}'.format(
-                    indent, NEW, key, value['new_value']))
-                result.append('{}{} {}: {}'.format(
-                    indent, LOST, key, value['old_value']))
-            else:
-                result.append('{}{} {}: {}'.format(
-                    indent, value['type'], key, get_value(value, level)))
+    for key, value in data.items():
+        if value.get('type') == CHILDREN:
+            result.append('{}{} {}: {{'.format(indent, SAME, key))
+            result.append(render(value['value'], level + 2))
+            result.append('{}}}'.format(indent + INDENT))
+        elif value.get('type') == CHANGED:
+            result.append('{}{} {}: {}'.format(
+                indent, NEW, key, value['new_value']))
+            result.append('{}{} {}: {}'.format(
+                indent, LOST, key, value['old_value']))
+        else:
+            result.append('{}{} {}: {}'.format(
+                INDENT * level, value['type'], key, get_value(value, level)))
     if level == 1:
         result.append('}')
     return '\n'.join(result)
@@ -90,15 +92,11 @@ def render(item, level=1):
 
 def get_value(value, level=1):
     res = ''
-    indent = INDENT * level
     if isinstance(value, dict) and 'type' not in value:
         res += ('{\n')
         for k, v in value.items():
-            res += ('{}{}: {}\n'.format(2 * indent, k, v))
-        if level >= 2:
-            res += ('{}}}'.format(indent + INDENT * (level - 2)))
-        else:
-            res += ('{}}}'.format(indent))
+            res += ('{}{}: {}\n'.format(INDENT * (level + 2), k, v))
+            res += ('{}}}'.format(INDENT * level))
     elif isinstance(value, dict) and 'type' in value:
         res = get_value(value['value'], level + 1)
     else:
